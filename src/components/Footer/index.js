9ed5117e79947c7de20b2477d93acc8caddf8e02
Breno Alves <breno550@hotmail.com>
2020-09-12 15:54:33 -0300

    project structure

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer-container">
+            <p>Desenvolvido por Breno e Guilherme</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+function Home() {
+    return (
+        <>
+            <Header />
+            <h1>TESTE</h1>
+            <Footer />
+        </>
+    )
+}
+
+export default Home;

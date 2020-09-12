7b5c4cf22397d2a5de9260548cba50f2163b8e0a
Guilherme <guilhermemoreira@ufrn.edu.br>
2020-09-12 18:06:41 -0300

    inicialização da header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import './styles.css';
+import Logo from '../../assets/Logo.png';
 
 const Header = () => {
+
+    useEffect(() => {
+        window.onscroll = () => {
+            const headerContainer = document.querySelector('header');
+
+            if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+                headerContainer.classList.add('header-open');
+            } else {
+                headerContainer.classList.remove('header-open');
+            }
+        }
+    });
+
     return (
-        <header className="header-container">
-            <h1>teste</h1>
+        <header className="header">
+            <div className="header-container">
+                <img className="logo" src={Logo}></img>
+                <ul className="menu">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Clientes</a></li>
+                    <li><a href="#">Orçamento</a></li>
+                    <li><a href="#">FAQ</a></li>  
+                </ul>
+            </div>
         </header>
     )
 }
